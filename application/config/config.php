1bd2bb0921e3f58ba339f5bceed6ab59e42c1ee3
musa <musafitriyadi@gmail.com>
2020-06-21 15:14:33 +0700

    login views

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kemdikbud/';
+$config['base_url'] = 'http://localhost:8080/kemdikbud/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -67,7 +67,14 @@
   <div class="container h-100">
     <div class="row h-100 justify-content-center align-items-center">
       <form class="col-md-4" id="formulir" method="post" action="">
-        <img src="<?= base_url('assets/image/logo/logo.png') ?>" alt="logo" class="w-100 mb-10">
+        <div class="row">
+          <div class="col col-md-4">
+            <img src="<?= base_url('assets/img/Kemdikbud.png') ?>" alt="logo" class="w-100 mb-10">
+          </div>
+          <div class="col">
+            <h3>Kementrian Pendidikan dan Kebudayaan</h3>
+          </div>
+        </div>
         <hr>
         <div class="form-group">
           <input autocomplete="off" type="text" class="form-control" id="username" onkeyup="cekbutton()" placeholder="Username" name="username" required="">
@@ -76,7 +83,7 @@
           <input autocomplete="off" type="password" data-toggle="password" class="form-control" id="password" onkeyup="cekbutton()" placeholder="Password" name="password" required="">
         </div>
 
-        <button type="submit" id="login" class="btn btn-secondary w-100 waves">Login</button>
+        <button type="submit" id="login" class="btn btn-primary w-100 waves">Login</button>
       </form>
     </div>
   </div>
@@ -87,86 +94,6 @@
   <script src="<?= base_url('assets/showpassword/bootstrap-show-password.min.js') ?>"></script>
 
   <!-- custom js -->
-  <script src="<?= base_url("assets/geetest/gt.js") ?>"></script>
-  <script>
-    var captcha = 0;
-    var handlerEmbed = function(captchaObj) {
-      $("#login").click(function(e) {
-        var validate = captchaObj.getValidate();
-        if (!validate) {
-          $(document).ready(function() {
-            const Toast = Swal.mixin({
-              toast: true,
-              position: 'top',
-              showConfirmButton: false,
-              timer: 2000
-            });
-
-            Toast.fire({
-              type: 'error',
-              title: 'Chaptcha tidak valid'
-            })
-          });
-          e.preventDefault();
-        }
-      });
-      // 将验证码加到id为captcha的元素里，同时会有三个input的值：geetest_challenge, geetest_validate, geetest_seccode
-      captchaObj.appendTo("#embed-captcha");
-      captchaObj.onReady(function() {
-        $("#wait")[0].className = "hide";
-      }).onSuccess(function() {
-        captcha = 1;
-        cekbutton();
-      }).onError(function() {
-        captcha = 0;
-      })
-      // 更多接口参考：http://www.geetest.com/install/sections/idx-client-sdk.html
-    };
-    $.ajax({
-      // 获取id，challenge，success（是否启用failback）
-      url: "./login/verifikasi?t=" + (new Date()).getTime(), // 加随机数防止缓存
-      type: "get",
-      dataType: "json",
-      success: function(data) {
-        // 使用initGeetest接口
-        // 参数1：配置参数
-        // 参数2：回调，回调的第一个参数验证码对象，之后可以使用它做appendTo之类的事件
-        initGeetest({
-          width: '100%',
-          lang: 'id',
-          gt: data.gt,
-          challenge: data.challenge,
-          new_captcha: data.new_captcha,
-          product: "embed", // 产品形式，包括：float，embed，popup。注意只对PC版验证码有效
-          offline: !data.success // 表示用户后台检测极验服务器是否宕机，一般不需要关注
-          // 更多配置参数请参见：http://www.geetest.com/install/sections/idx-client-sdk.html#config
-        }, handlerEmbed);
-      }
-    });
-
-    function cekbutton() {
-      var username = document.getElementById("username").value;
-      var password = document.getElementById("password").value;
-      if (password != "") {
-        if (username == "" || password == "" || captcha == 0) {
-          var login = document.getElementById("login");
-          login.classList.add("btn-secondary");
-          login.classList.remove("btn-primary");
-          login.disabled = true;
-        } else {
-          var login = document.getElementById("login");
-          login.classList.add("btn-primary");
-          login.classList.remove("btn-secondary");
-          $("#login").removeAttr("disabled");
-        }
-      } else {
-        var login = document.getElementById("login");
-        login.classList.add("btn-secondary");
-        login.classList.remove("btn-primary");
-        login.disabled = true;
-      }
-    }
-  </script>
   <script>
     $(".waves").mousedown(function(e) {
       var button = e.target;
@@ -194,26 +121,6 @@
       return false;
     });
   </script>
-
-  <!-- notification -->
-  <?php if ($this->session->flashdata('user_pass_salah') == "aktif") { ?>
-    <script>
-      $(document).ready(function() {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: 'top',
-          showConfirmButton: false,
-          timer: 5000
-        });
-
-        Toast.fire({
-          type: 'error',
-          title: 'Password dan Username Salah'
-        })
-      });
-    </script>
-  <?php } ?>
-
 </body>
 
 </html>
