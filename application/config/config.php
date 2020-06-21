7f4e0e9733a9bd98495bfdfa270af8486b6d4774
musa <musafitriyadi@gmail.com>
2020-06-21 14:54:35 +0700

    hilang index.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ult_kemdikbud/';
+$config['base_url'] = 'http://localhost:8080/kemdikbud/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/ult_kemdikbud/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
