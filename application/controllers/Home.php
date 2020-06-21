dea5a5a173f8a511df1fdc7ac50e6236bfc7650e
lutfi-GRNJG <60430010+lutfi-GRNJG@users.noreply.github.com>
2020-06-21 09:37:47 +0700

    htaccess

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,4 +3,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
+    public function index()
+    {
+        $this->load->view('welcome_message');
+    }
 }
