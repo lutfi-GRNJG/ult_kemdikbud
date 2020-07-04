90ad6da53f3a6a1834c5bde102743eca60063d99
musa <musafitriyadi@gmail.com>
2020-07-04 14:16:00 +0700

    what about us

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -11,7 +11,7 @@
                             <a class="dropdown-item" href="#">Settings</a>
                             <a class="dropdown-item" href="#">Activity Log</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="login.html">Logout</a>
+                            <a class="dropdown-item" href="<?= site_url('login/logout') ?>">Logout</a>
                         </div>
                     </li>
                 </ul>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -54,7 +54,6 @@
 					<li><a href="#about">About</a></li>
 					<li><a href="#more-services">Layanan</a></li>
 					<li><a href="#prosedur">Prosedur</a></li>
-					<li><a href="#link">Link Terkait</a></li>
 
 
 					<li class="get-started"><a href="<?= site_url('Login') ?>">Login</a></li>
@@ -323,152 +322,6 @@
 		<!-- End Procedure Section -->
 
 
-		<!-- ======= F.A.Q Section ======= -->
-		<section id="link" class="faq">
-			<div class="container">
-
-				<div class="section-title" data-aos="fade-up">
-					<h2>Link Terkait</h2>
-				</div>
-
-				<div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
-					<div class="col-lg-5">
-						<i class="ri-check-double-fill"></i>
-						<h4>Kementrian Pendidikan dan Kebudayaan</h4>
-					</div>
-					<div class="col-lg-7">
-						<p>
-							Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-						</p>
-					</div>
-				</div><!-- End F.A.Q Item-->
-
-				<div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
-					<div class="col-lg-5">
-						<i class="ri-question-line"></i>
-						<h4>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h4>
-					</div>
-					<div class="col-lg-7">
-						<p>
-							Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
-						</p>
-					</div>
-				</div><!-- End F.A.Q Item-->
-
-				<div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
-					<div class="col-lg-5">
-						<i class="ri-question-line"></i>
-						<h4>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?</h4>
-					</div>
-					<div class="col-lg-7">
-						<p>
-							Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus.
-						</p>
-					</div>
-				</div><!-- End F.A.Q Item-->
-
-				<div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
-					<div class="col-lg-5">
-						<i class="ri-question-line"></i>
-						<h4>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</h4>
-					</div>
-					<div class="col-lg-7">
-						<p>
-							Aperiam itaque sit optio et deleniti eos nihil quidem cumque. Voluptas dolorum accusantium sunt sit enim. Provident consequuntur quam aut reiciendis qui rerum dolorem sit odio. Repellat assumenda soluta sunt pariatur error doloribus fuga.
-						</p>
-					</div>
-				</div><!-- End F.A.Q Item-->
-
-				<div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="500">
-					<div class="col-lg-5">
-						<i class="ri-question-line"></i>
-						<h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
-					</div>
-					<div class="col-lg-7">
-						<p>
-							Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
-						</p>
-					</div>
-				</div><!-- End F.A.Q Item-->
-
-			</div>
-		</section><!-- End F.A.Q Section -->
-
-		<!-- ======= Contact Section ======= -->
-		<section id="contact" class="contact">
-			<div class="container">
-
-				<div class="section-title" data-aos="fade-up">
-					<h2>Contact Us</h2>
-				</div>
-
-				<div class="row">
-
-					<div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-						<div class="contact-about">
-							<h3>Vesperr</h3>
-							<p>Cras fermentum odio eu feugiat. Justo eget magna fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-							<div class="social-links">
-								<a href="#" class="twitter"><i class="icofont-twitter"></i></a>
-								<a href="#" class="facebook"><i class="icofont-facebook"></i></a>
-								<a href="#" class="instagram"><i class="icofont-instagram"></i></a>
-								<a href="#" class="linkedin"><i class="icofont-linkedin"></i></a>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-3 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
-						<div class="info">
-							<div>
-								<i class="ri-map-pin-line"></i>
-								<p>A108 Adam Street<br>New York, NY 535022</p>
-							</div>
-
-							<div>
-								<i class="ri-mail-send-line"></i>
-								<p>info@example.com</p>
-							</div>
-
-							<div>
-								<i class="ri-phone-line"></i>
-								<p>+1 5589 55488 55s</p>
-							</div>
-
-						</div>
-					</div>
-
-					<div class="col-lg-5 col-md-12" data-aos="fade-up" data-aos-delay="300">
-						<form action="forms/contact.php" method="post" role="form" class="php-email-form">
-							<div class="form-group">
-								<input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-								<div class="validate"></div>
-							</div>
-							<div class="form-group">
-								<input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-								<div class="validate"></div>
-							</div>
-							<div class="form-group">
-								<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-								<div class="validate"></div>
-							</div>
-							<div class="form-group">
-								<textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-								<div class="validate"></div>
-							</div>
-							<div class="mb-3">
-								<div class="loading">Loading</div>
-								<div class="error-message"></div>
-								<div class="sent-message">Your message has been sent. Thank you!</div>
-							</div>
-							<div class="text-center"><button type="submit">Send Message</button></div>
-						</form>
-					</div>
-
-				</div>
-
-			</div>
-		</section><!-- End Contact Section -->
-
 	</main><!-- End #main -->
 
 	<!-- ======= Footer ======= -->
@@ -477,22 +330,17 @@
 			<div class="row d-flex align-items-center">
 				<div class="col-lg-6 text-lg-left text-center">
 					<div class="copyright">
-						&copy; Copyright <strong>Vesperr</strong>. All Rights Reserved
+						&copy; Copyright <strong>Kemdikbud</strong>. All Rights Reserved
 					</div>
 					<div class="credits">
-						<!-- All the links in the footer should remain intact. -->
-						<!-- You can delete the links only if you purchased the pro version. -->
-						<!-- Licensing information: https://bootstrapmade.com/license/ -->
-						<!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/vesperr-free-bootstrap-template/ -->
-						Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
 					</div>
 				</div>
 				<div class="col-lg-6">
 					<nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
 						<a href="#intro" class="scrollto">Home</a>
 						<a href="#about" class="scrollto">About</a>
-						<a href="#">Privacy Policy</a>
-						<a href="#">Terms of Use</a>
+						<a href="#more-services" class="scrollto">Layanan</a>
+						<a href="#prosedur" class="scrollto">Prosedur</a>
 					</nav>
 				</div>
 			</div>
