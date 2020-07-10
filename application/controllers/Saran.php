36d4a7189333c1ce426b4208097a667e16968e11
Rizqa Tsaqila Aulia Haq <rizqatsaqila@gmail.com>
2020-07-10 19:46:12 +0700

    layanan saran

--- application/controllers/Saran.php
+++ application/controllers/Saran.php
@@ -5,6 +5,27 @@ class Saran extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('pengaduan/form_pengaduan.php');
+        $this->load->view('saran/form_saran.php');
+    }
+
+    public function tambah_saran()
+    {
+        $data =
+            [
+                'alamat_email' => $this->input->post('email'),
+                'nama_pelapor' => $this->input->post('nama'),
+                'alamat_rumah' => $this->input->post('alamat'),
+                'nomor_telepon' => $this->input->post('telp'),
+                'nomor_ktp' => $this->input->post('ktp'),
+                'profesi' => $this->input->post('profesi'),
+                'provinsi' => $this->input->post('provinsi'),
+                'kabupaten' => $this->input->post('kabupaten'),
+                'kecamatan' => $this->input->post('kecamatan'),
+                'judul_laporan' => $this->input->post('judul'),
+                'saran' => $this->input->post('saran'),
+            ];
+
+        $this->db->insert('', $data);
+        redirect('Home');
     }
 }
